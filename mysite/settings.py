20e94b435fe1b9ac78995ad2a44d8d458d62d027
lena <lena.smagina.00@mail.ru>
2021-06-24 23:02:50 +1000

    Changed the HTML for the site.

--- mysite/settings.py
+++ mysite/settings.py
@@ -25,7 +25,7 @@ SECRET_KEY = 'django-insecure-yy)*j)+6loa04aj4(vg$+b+b0r+)(out=k(0t2+z-x1!tzu+j1
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ['*']
 
 
 # Application definition
